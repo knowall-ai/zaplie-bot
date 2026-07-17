f68b453470aec46922a70ab2029905933018e4e7
wkatir <wilmerhenrysalazarmartinez@gmail.com>
2026-07-17 16:35:15 -0600

    fix(tabs): make the Your week page use the full page width like the other pages

--- tabs/src/Week.tsx
+++ tabs/src/Week.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 const Week: React.FC = () => {
   return (
-    <div style={{ background: '#1F1F1F', marginTop: 20, marginBottom: 40 }}>
+    <div style={{ background: '#1F1F1F', marginTop: 20, marginBottom: 40, alignSelf: 'stretch', width: '100%', boxSizing: 'border-box' }}>
       <div style={{ paddingLeft: 20, paddingRight: 20, paddingBottom: 20, paddingTop: 0 }}>
         <WeekComponent />
       </div>
